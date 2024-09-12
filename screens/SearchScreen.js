0979c69249365d963a972b17d45817f5c57d580a
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-09-12 22:35:43 +0300

    Initialised useState

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -5,6 +5,13 @@ const API_URL = 'https://app.mymovies.africa/api/cache';
 
 const SearchScreen = () =>  {
 
+    const [movies, setMovies] = useState([]);
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+
+
 };
 
 export default SearchScreen;
