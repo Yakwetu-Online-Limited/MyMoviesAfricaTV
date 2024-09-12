4cbb5a85f5b9ca3cbc0391ce1cd704aaf98541b7
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-09-12 23:19:32 +0300

    Handled the navigation to the search

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StatusBar, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SearchScreen from './screens/SearchScreen';
+import HomeScreen from './screens/HomeScreen'; 
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Home Screen' }}
+        />
+        <Stack.Screen 
+          name="Search" 
+          component={SearchScreen} 
+          options={{
+            headerStyle: {
+              backgroundColor: '#000', // Black background for header
+            },
+            headerTitleStyle: {
+              color: 'transparent', // Hide the title text
+            },
+            headerTintColor: '#fff', // Color for back button
+            headerTitleAlign: 'center', // Center the back button
+            headerShown: true, // Show the header
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
