e3f4e013bfac68caa554e7e82f7664c728abe8cc
Antony <kantony.njiru@gmail.com>
2024-09-12 20:45:27 +0300

    fixing of secret key

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDElGZdR65WAMLQZ3vNMIYr0xZ7BWjZ6JI",
-  authDomain: "my-movies-79784.firebaseapp.com",
-  projectId: "my-movies-79784",
-  storageBucket: "my-movies-79784.appspot.com",
-  messagingSenderId: "500064335145",
-  appId: "1:500064335145:web:f1f4bb7cf949549f4ab338",
-  measurementId: "G-S1MB6L1WVS"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
