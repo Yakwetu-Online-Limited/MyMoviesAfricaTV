81feee3c05182d4fcb4ea0e020e2b9aad1c9cefd
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-09-12 22:32:35 +0300

    Exported SearchSreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,3 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, FlatList, ActivityIndicator, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const API_URL = 'https://app.mymovies.africa/api/cache';
+
+const SearchScreen = () =>  {
+
+};
+
+export default SearchScreen;
