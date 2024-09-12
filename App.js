80294a82e068eaf2d8d88fc572dd8454d1e43e8a
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-09-12 14:50:12 +0300

    Create movieDeatilScreen and add basic structure.

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MovieDetailScreen from './screens/MovieDetailScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="MovieDetail"
+        screenOptions={{
+          headerShown: true, // Show the header by default
+        }}
+      >
+      <Stack.Screen
+          name="MovieDetail"
+          component={MovieDetailScreen}
+          options={{ title: 'Movie Details' }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
