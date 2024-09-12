7470f8194563634c66163dc52b1a33e3eab0a503
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-09-12 22:37:31 +0300

    Fetched all the movies

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -11,6 +11,32 @@ const SearchScreen = () =>  {
     const [error, setError] = useState(null);
     const [searchQuery, setSearchQuery] = useState('');
 
+    const fetchMovies = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            console.log('API Response:', data);
+
+            // Check if 'content' key exists and is an array
+            if (data.content && Array.isArray(data.content)) {
+                setMovies(data.content);
+                setFilteredMovies(data.content);
+            } else {
+                setMovies([]);
+                setFilteredMovies([]);
+                setError('Unexpected data format.');
+            }
+        } catch (err) {
+            console.error('Error fetching movies:', err);
+            setError('Failed to fetch movies.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
 
 };
 
