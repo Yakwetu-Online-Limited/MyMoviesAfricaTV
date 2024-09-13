9b1c1ff7243c4bb2875dc9d6f78fb5202b43830e
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-09-13 08:58:57 +0300

    Set up a function to fetch movie details.

--- screens/MovieDetailScreen.js
+++ screens/MovieDetailScreen.js
@@ -1,11 +1,43 @@
 import React, { useState, useEffect }from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 import { API_URL } from '../store';
 
 const MovieDetailScreen = () => {
     // set useStates
     const [ movie, setMovie ] = useState(null);
     const [ loading, setLoading ]= useState(true);
+    const [ error, setError ] = useState(null);
+
+    const movieId = '180'; // Static ID for testing
+
+    // Fetch the movie data from the API
+    useEffect(() => {
+        //Function to fetch movie data from the api
+        const fetchMovieData = async() => {
+            try {
+                const response = await fetch (API_URL);
+                const data = await response.json();
+                const movieData = data.content.find(movie => movie.id === movieId);
+                if (!movieData) {
+                    throw new Error ('Movie not found');
+                }
+                setMovie(movieData);
+            } catch(error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchMovieData();
+    }, []);
+    // Render loading state.
+    if (loading) {
+        return <ActivityIndicator size = "large" color="#0000ff" />
+    }
+    // Render error state
+    if (error) {
+        return <Text>Error fetching movie data: {error}</Text>
+    }
 
 
 
