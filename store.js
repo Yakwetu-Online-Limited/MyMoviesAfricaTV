3ba9da722c01b8ccff603ac7574732e75688d13b
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-09-13 10:22:02 +0300

    Add the media api URL to display images.

--- store.js
+++ store.js
@@ -1,3 +1,7 @@
-const API_URL = 'https://app.mymovies.africa/api/cache';
+export const API_URL = 'https://app.mymovies.africa/api/cache';
 
-export default API_URL;
+export const baseURL = 'https://api.mymovies.africa/';
+
+export const mediaURL = baseURL + 'content/uploads/';
+
+
