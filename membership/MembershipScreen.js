791ab01ad3553a28204e760739e20dc4b0dd2c80
jolene <sitienijolene@gmail.com>
2024-09-15 23:34:18 +0300

    create the header section containing logo and log out

--- membership/MembershipScreen.js
+++ membership/MembershipScreen.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native';
+//import { Button } from 'react-native-elements';
 
 const MembershipScreen = () => {
     return (
-        <View>
-            <Text>MembershipScreen</Text>
-        </View>
+        <SafeAreaView style={styles.safeArea}>
+            {/* header section */}
+            <View style={styles.header}>
+                <View >
+                    <Image 
+                        source = {require('../assets/mymovies-africa-logo.png')}
+                        style={styles.logoImage}
+                    />
+                </View>
+
+                {/* logging out */}
+                <View>
+                    <TouchableOpacity>
+                        <Image 
+                            source={require('../assets/default.jpg')}
+                            style={styles.logoutImage}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
+
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#000000',
+    },
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    logoImage: {
+        width: 400,
+        height: 60,
+    },
+    logoutImage: {
+        width: 50,
+        height: 50, 
+    },                  
+
+    })         
+
+
+
 
 export default MembershipScreen;
