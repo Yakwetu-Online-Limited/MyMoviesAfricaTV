5787eafd346d19e705c172426bda00911430be78
jolene <sitienijolene@gmail.com>
2024-09-15 22:40:59 +0300

    create the stack navigation for membership screen

---
+++ membership/MembershipScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MembershipScreen = () => {
+    return (
+        <View>
+            <Text>MembershipScreen</Text>
+        </View>
+    );
+};
+
+export default MembershipScreen;
