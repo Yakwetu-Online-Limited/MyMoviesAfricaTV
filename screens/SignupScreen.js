ab2b8f34601d5bea36a1cf8a4133e20b0406d68c
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-09-16 20:04:06 +0300

    passing username via navigation

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -26,7 +26,7 @@ const SignupScreen = ({ navigation }) => {
       .then((userCredential) => {
         // User created successfully, you can save additional information (like fullName, phoneNumber) here.
         Alert.alert('Success', 'Account created successfully');
-        navigation.navigate('Login');
+        navigation.navigate('Collection', {username: fullName});
       })
       .catch((error) => {
         setIsLoading(false);
