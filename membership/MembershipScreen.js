9fa2a9863472ad5576740191830258df823e055b
jolene <sitienijolene@gmail.com>
2024-09-18 11:01:47 +0300

    create the card section containing the memeber's details

--- membership/MembershipScreen.js
+++ membership/MembershipScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native';
-//import { Button } from 'react-native-elements';
+import { Button, Card } from '@rneui/themed';
 
 const MembershipScreen = () => {
     return (
@@ -24,6 +24,56 @@ const MembershipScreen = () => {
                     </TouchableOpacity>
                 </View>
             </View>
+
+            {/* container containing the body container */}
+            <Card containerStyle={{backgroundColor: '#000000', margin: 10}}>
+                <Card.Title style={{color: 'white'}}>
+                    <Image source= {require('../assets/default.jpg')} style={{width: 50, height: 50, borderRdius: 50}}/>
+                    <Text style={{color: 'white', fontWeight: 'bold'}}>Account</Text>
+                </Card.Title>
+
+                <Card.Divider/>
+
+                <View containerStyle={{padding: 10}}>
+                    <Card.Image source={require('../assets/default.jpg')} style ={styles.profileImage}/>
+                    <View>
+                        <Text style={{color: 'white'}}>Ryan Munge</Text>
+                        <Text style={{color: 'white'}}>ryanmunge@gmail.com</Text> 
+                        <Text style={{color: 'white'}}>+254701449264</Text>
+                        <Text style={{color: 'white', fontWeight: 'bold', textDecorationLine: 'underline'}}>Wallet Balance</Text>
+                        <Text style={{color: 'white'}}>KSH 0</Text>
+                    </View>
+
+                    <Button
+                        title="Top Up"
+                        buttonStyle={{
+                            borderColor: 'rgba(78, 116, 289, 1)',
+                        }}
+                        type="outline"
+                        titleStyle={{ color: 'rgba(78, 116, 289, 1)' }}
+                        containerStyle={{
+                            width: 200,
+                            marginHorizontal: 50,
+                            marginVertical: 10,
+                        }}
+                    />
+
+                    <Button
+                        title="Update Account"
+                        buttonStyle={{
+                            borderColor: 'rgba(78, 116, 289, 1)',
+                        }}
+                        type="outline"
+                        titleStyle={{ color: 'rgba(78, 116, 289, 1)' }}
+                        containerStyle={{
+                            width: 200,
+                            marginHorizontal: 50,
+                            marginVertical: 10,
+                        }}
+                    />
+                </View>
+
+            </Card>
         </SafeAreaView>
     );
 };
@@ -47,7 +97,11 @@ const styles = StyleSheet.create({
     logoutImage: {
         width: 50,
         height: 50, 
-    },                  
+    },
+    profileImage: {
+        borderRadius: 100,
+    }
+
 
     })         
 
